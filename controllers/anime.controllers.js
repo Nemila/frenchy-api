465288576b511712bd509717280f5409ba529f0f
Lamine <pridila.2006@gmail.com>
2023-01-13 15:20:12 +0100

    remove getAnime, getAnimeVf, getBestScore

--- controllers/anime.controllers.js
+++ controllers/anime.controllers.js
@@ -1,19 +1,19 @@
 const api = require("anime-vostfr");
 const asyncHandler = require("express-async-handler");
 
-const getAnime = (req, res) => res.status(200).json(req.data.slice(0, 60));
-const getAnimeVF = (req, res) => res.status(200).json(req.dataVF.slice(0, 60));
+// const getAnime = (req, res) => res.status(200).json(req.data.slice(0, 60));
+// const getAnimeVF = (req, res) => res.status(200).json(req.dataVF.slice(0, 60));
+
 const getPopularAnime = (req, res) => {
-  // const { vf } = req.body;
   const result = api.popularAnime(req.dataVF);
   res.status(200).json(result.slice(0, 60));
 };
 
-const getBestScore = (req, res) => {
-  const { vf } = req.body;
-  const result = api.bestScoreAnime(vf ? req.dataVF : req.data);
-  res.status(200).json(result.slice(0, 60));
-};
+// const getBestScore = (req, res) => {
+//   const { vf } = req.body;
+//   const result = api.bestScoreAnime(vf ? req.dataVF : req.data);
+//   res.status(200).json(result.slice(0, 60));
+// };
 
 const findAnime = (req, res) => {
   const { query } = req.params;
@@ -52,11 +52,8 @@ const getEpisode = asyncHandler(async (req, res) => {
 });
 
 module.exports = {
-  getAnime,
-  getAnimeVF,
   getPopularAnime,
   findAnime,
   getInfo,
   getEpisode,
-  getBestScore,
 };
--- routes/anime.router.js
+++ routes/anime.router.js
@@ -1,21 +1,14 @@
 const express = require("express");
 const {
-  getAnime,
-  getAnimeVF,
   findAnime,
   getInfo,
   getEpisode,
   getPopularAnime,
-  getBestScore,
 } = require("../controllers/anime.controllers");
 
 const router = express.Router();
 
-router.get("/vo", getAnime);
-router.get("/vf", getAnimeVF);
 router.get("/popular", getPopularAnime);
-router.get("/bestScore", getBestScore);
-
 router.get("/search/:query", findAnime);
 router.get("/info/:id", getInfo);
 router.get("/episode/:id", getEpisode);
