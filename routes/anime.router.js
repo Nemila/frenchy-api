f3c299a53ebfa50b7725817537c59727682a6b9d
Lamine <pridila.2006@gmail.com>
2023-01-13 15:26:00 +0100

    .

--- routes/anime.router.js
+++ routes/anime.router.js
@@ -7,7 +7,6 @@ const {
 } = require("../controllers/anime.controllers");
 
 const router = express.Router();
-
 router.get("/popular", getPopularAnime);
 router.get("/search/:query", findAnime);
 router.get("/info/:id", getInfo);
